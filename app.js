0f25e7918a114a5903e0bbfaba745dd39b29acd6
Ubuntu <ubuntu@ip-172-31-47-200.ap-northeast-2.compute.internal>
2021-04-26 04:21:14 +0000

    db구축

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 // view engine setup
-app.set('port', process.env.OIRT || 4000);
+app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,18 @@
+// import db_config from '/model/conn.js'
 var express = require('express');
 var router = express.Router();
+var db_config = require('../model/conn');
+var connection = db_config.init();
+console.log("test1");
+connection.query('SELECT * FROM user', function(err, results, fields) {
+  if (err) {
+    console.log(err);
+    console.log("test2");
+  }
+  console.log(results);
+});
 
+connection.end();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
