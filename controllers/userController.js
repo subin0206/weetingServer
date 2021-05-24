45821aa0d743699778fda64144db48d2b4c452f9
LAPTOP-TO0U6FQM\Hello <2149ksb@gmail.com>
2021-05-24 20:03:38 +0900

    회원가입 오류

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,12 @@
 const userModel = require('../model/userModel');
 console.log("connect router");
+const nodemailer = require('nodemailer');
 
+function randomToken(){
+    var token = '';
+    token = Math.random().toString(36).substr(2,11); // "twozs5xfni"
+    return token;
+}
 exports.getEmail = (req, res)=>{
     console.log("connect router2");
     let email = {'email':req.body.email};
@@ -25,7 +31,78 @@ exports.getEmail = (req, res)=>{
     });
 };
 
+exports.authEmail = (req, res, next) =>{
+
+    let email = req.body.email;
+    console.log(email);
+    var token = randomToken();
+    let smtpTransport = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: "",
+            pass: ""
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let mailOptions = {
+        from: "2149ksb@gmail.com",
+        to: email,
+        subject: "Weeting 가입 인증",
+        html: '<p>안녕하세요.<br>여성들의 안전한 취미 공유 서비스 "위팅"입니다.<br>아래의 인증번호를 입력해 이메일 인증을 완료해주세요 !</p><br><br>' +
+          "<b>인증번호: "+token+"</b>"
+
+      };
+      
+    console.log(token);
+    smtpTransport.sendMail(mailOptions, (error, responses) =>{
+        if(error){
+            console.log(error.console);
+            res.json({
+                'state': 400,
+                'message':'이메일 발신 실패'
+                });
+        }else{
+            res.json({
+                'state': 200,
+                'token': token,
+                'message':'이메일 발신 성공'
+                });
+          
+        }
+        smtpTransport.close();
+    });
+};
+exports.joinUser = (req, res) =>{
+    let item = {
+        'email': req.body.email,
+        'age': req.body.age,
+        'name': req.body.name,
+        'interests' : req.body.interests,
+        'description' : req.body.description,
+        'pwd' : req.body.pwd
+    };
+    userModel.insertUser(item, result => {
+        if(error) {
+            res.json({
+            'state': 400,
+            'message':'회원가입 실패'
+            });
+            console.error('error', error);
+        }
+        else{
+            res.json({
+                'state': 200,
+                'message':'회원가입 성공'
+                });
+        }
+    });        
+
+};
 exports.main = (req, res) => {
     console.log("잘 되니");
     res.send('respond with a resource');
 }
+
--- model/userModel.js
+++ model/userModel.js
@@ -1,22 +1,9 @@
 // mysql 연결 
 const db_config = require('../model/conn');
 const connection = db_config.init();
+const bcrypt = require('bcrypt'); 
+const saltRound = 10;
 
-// exports.selectId = (results, id) =>{
-//     let sql = 'select * from user where user_id = ?';
-//     let bindParm = [results.user_id];
-
-//     connection.query(sql, bindParam, (err, results, fields) => { 
-//         if (err) { 
-//             console.error('Error code : ' + err.code); 
-//             console.error('Error Message : ' + err.message); 
-//             throw new Error(err); 
-//         } 
-//             else { 
-//                 results(JSON.parse(JSON.stringify(results))); 
-//             } 
-//         });
-// };
 exports.selectEmail = (data, result) => {
     let sql = 'select user_id from user where email = ?';
     // let bindParam = [data.email];
@@ -31,7 +18,39 @@ exports.selectEmail = (data, result) => {
                 console.log(results[0]);
             } 
         });
-
-
 };
+
+exports.insertUser = (data, result) => {
+    bcrypt.genSalt(saltRound, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(data.pwd, salt, (err, hash) => {
+            if (err) return next(err);
+        console.log(result);
+
+        let sql = 'insert into user (email, age, name, interests, description, pwd) values(?,?,?,?,?,?)';
+        let bindParam = [ 
+            data.email,
+            data.age,
+            data.name,
+            data.interests,
+            data.description,
+            hash
+        ];
+        console.log(data, "data");
+        connection.query(sql,bindParam, (err, results, fields) => { 
+            if (err) { 
+                console.error('Error code : ' + err.code); 
+                console.error('Error Message : ' + err.message); 
+                throw new Error(err.message); 
+            } 
+                else { 
+                  result({
+                      'state':200
+                  })                      
+                } 
+            });
+        });
+    });
+};
+
 
