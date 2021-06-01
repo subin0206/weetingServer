c7e75a31992f464be749792cb9e59c96a891a2df
sonjuh1234 <sonjuh1234@gmail.com>
2021-06-01 21:41:53 +0900

    뭐지

---
+++ config/database.js
@@ -0,0 +1,14 @@
+// var db_info = {
+//     host: 'ec2-54-180-107-241.ap-northeast-2.compute.amazonaws.com',
+//     port: '3305',
+//     user: 'weeting',
+//     password: 'Weeting!',
+//     database: 'Weeting'
+// };
+module.exports = {
+    db_info:function(){
+        return {
+            host: 'localhost',
+        };
+    }
+}
